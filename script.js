ec7c762092bc232a221cdd9e3599c2134ed1c4d0
kaitlinkenney <kaitlin.kenney96@gmail.com>
2020-10-17 19:48:10 -0400

    third commit

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ console.log("this is working");
 
 function displayWeatherInfo() {
 
-    var city = $(this).attr("")
+    var city = $(this).attr("weatherdata")
     var queryURL = "api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=fd9e64d7b57ef3d61cfb920579f1e31f";
 
     $.ajax({
@@ -12,16 +12,30 @@ function displayWeatherInfo() {
         console.log(response);
 
         var weatherDiv = $("<div class='weather'>");
+        
+        var temp = reponse.temp;
+        var pOne = $("<p>").text("Temperature: " + temp);
+        weatherDiv.append(pOne);
 
+        var humidity = response.humidity;
+        var pTwo = $("<p>").text("Humidity: " + humidity);
+        weatherDiv.append(pTwo);
 
+        var windSpeed = response.wind.speed;
+        var pThree = $("<p>").text("Wind speed: " + windSpeed);
+        weatherDiv.append(pThree);
 
-    
-    })
+        var uvIndex = response.
+
+    });
 }
 
 
 $("#search").on("click", function(event){
     event.preventDefault();
 
+    var cityDiv = $("<div class='cities'>");
     var city = ("#city-input").val().trim();
+
+    cityDiv.prepend(city);
 });
