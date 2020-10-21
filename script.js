2678ba410c9115f34e67e116fe360e532af7fa40
kaitlinkenney <kaitlin.kenney96@gmail.com>
2020-10-21 15:23:48 -0400

    farenheit conversion

--- script.js
+++ script.js
@@ -17,41 +17,53 @@ function displayCurrent(city) {
 
         var weatherDiv = $("<div class='weather'>");
 
+        // var currentDate = moment().format("L");
+        // weatherDiv.append(currentDate);
+
+        var iconUrl = response.weather[0].icon;
+        // var iconImage = $("<img>");
+        // iconImage.attr("src", iconUrl);
+        weatherDiv.append(iconUrl);
+
         var temp = response.main.temp;
-        var pOne = $("<p>").text("Temperature: " + temp);
+        var tempF = (response.main.temp - 273.15) * 1.80 + 32;
+        var pOne = $("<p>").text("Temperature: " + tempF.toFixed(2) + " Farenheit");
         weatherDiv.append(pOne);
 
         var humidity = response.main.humidity;
-        var pTwo = $("<p>").text("Humidity: " + humidity);
+        var pTwo = $("<p>").text("Humidity: " + humidity + "%");
         weatherDiv.append(pTwo);
 
         var windSpeed = response.wind.speed;
-        var pThree = $("<p>").text("Wind speed: " + windSpeed);
+        var pThree = $("<p>").text("Wind speed: " + windSpeed + " MPH");
         weatherDiv.append(pThree);
 
         $("#jumbo").append(weatherDiv);
         var lat = reponse.coord.lat;
         var lon = response.coord.lon;
+        
         displayUV(lat, lon);
     
     })
 };
 
-function displayUV (lat, lon){
-    var queryURL = "http://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + lon + "&appid=fd9e64d7b57ef3d61cfb920579f1e31f";
+// function displayUV (lat, lon){
+//     var queryURL = "http://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + lon + "&appid=fd9e64d7b57ef3d61cfb920579f1e31f";
 
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function (response) {
-        console.log(response);
+//     $.ajax({
+//         url: queryURL,
+//         method: "GET",
+//         dataType: "json"
+//     }).then(function (response) {
+//         console.log(response);
 
-        // var uvDiv = ($"<div class='uv'>");
-
-        var uvIndex = response.coord
+//         var uvDiv = $("<div class='uv'>");
+//         var pFour = $("<p>").text("UV Index: " + );
+//         uvDiv.append(pFour);
         
-    })
-}
+//         $("#jumbo").append(uvDiv);
+//     })
+// }
 
 
 function displayForecast(city) {
@@ -62,19 +74,24 @@ function displayForecast(city) {
         url: queryURL,
         method: "GET",
         dataType: "json"
-    });
-    // }).then(function (response) {
-    //     $("#cities").text(city);
-    // });
-    function searchWorks(){
-        //create history of search
-        if (previousCities.indexof(city) === -1){
-            previousCities.push(city);
-            var storedCities= localStorage.setItem("previousCities", JSON.stringify(previousCities));
-            console.log(storedCities);
-        }
+    }).then(function (response) {
+        console.log(response);
 
-    }
+        var forecastDiv = $("<div class='forecast'>");
+
+        var date = response.date
+
+
+    })
+    // function searchWorks(){
+    //     //create history of search
+    //     if (previousCities.indexof(city) === -1){
+    //         previousCities.push(city);
+    //         var storedCities= localStorage.setItem("previousCities", JSON.stringify(previousCities));
+    //         console.log(storedCities);
+    //     }
+
+    // }
 }
 
 var previousCities;
