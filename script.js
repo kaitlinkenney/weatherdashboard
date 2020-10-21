f4da966528ddc3b83b0e588fb0f9746c4025a5c4
kaitlinkenney <kaitlin.kenney96@gmail.com>
2020-10-21 00:03:35 -0400

    uv added

--- script.js
+++ script.js
@@ -8,11 +8,11 @@ function displayWeatherInfo() {
     $.ajax({
         url: queryURL,
         method: "GET"
-    }).then(function(response) {
+    }).then(function (response) {
         console.log(response);
 
         var weatherDiv = $("<div class='weather'>");
-        
+
         var temp = reponse.temp;
         var pOne = $("<p>").text("Temperature: " + temp);
         weatherDiv.append(pOne);
@@ -25,28 +25,63 @@ function displayWeatherInfo() {
         var pThree = $("<p>").text("Wind speed: " + windSpeed);
         weatherDiv.append(pThree);
 
-        var uvIndex = response.
+    
+    })
+};
 
-    });
-}
+// function displayUV (){
+//     var queryURL = http://api.openweathermap.org/data/2.5/uvi?lat={lat}&lon={lon}&appid=fd9e64d7b57ef3d61cfb920579f1e31f"
+
+//     $.ajax({
+//         url: queryURL,
+//         method: "GET"
+//     }).then(function (response) {
+//         console.log(response);
+
+//         var uvDiv = ($"<div class='uv'>");
+
+//         var uvIndex = response.
+        
+//     })
+// }
 
 
-$("#search").on("click", function(event){
-    event.preventDefault();
-
-    var cityDiv = $("<div class='cities'>");
-    var city = ("#city-input").val().trim();
-
+function searchClick() {
     var queryURL = "api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=fd9e64d7b57ef3d61cfb920579f1e31f";
 
     $.ajax({
         url: queryURL,
-        method: "GET"
-      }).then(function(response) {
-        $("#cities").text(JSON.stringify(response));
-      });
+        method: "GET"});
+    // }).then(function (response) {
+    //     $("#cities").text(city);
+    // });
+    function searchWorks(){
+        //create history of search
+        if (previousCities.indexof(city) === -1){
+            previousCities.push(city);
+            var storedCities= localStorage.setItem("previousCities", JSON.stringify(previousCities));
+            console.log(storedCities);
+        }
+
+    }
+}
+
+var previousCities;
 
 
 
-    cityDiv.prepend(city);
-});
+//on click for history that will run the searchClick.
+    $("#search").on("click", function (event) {
+        event.preventDefault();
+
+        var cityDiv = $("<div class='cities'>");
+    var city = $("#city-input").val().trim();
+        console.log(city);
+
+    searchClick(city);
+
+
+        cityDiv.prepend(city);
+    });
+
+    displayWeatherInfo();
